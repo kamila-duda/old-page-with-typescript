f4b14fee68eb337f8fd997450ffc4decb4fbd157
kamila-duda <kakus86@o2.pl>
2020-10-08 12:09:58 +0200

    Updates

---
+++ precache-manifest.f89d38d80c7727b58640ca541a7049bc.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50e2c9be255f788202b386e4c2315c7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e0fe107ae2bff8fb306",
+    "url": "/static/css/main.0e96c88f.chunk.css"
+  },
+  {
+    "revision": "33a92a897dc58ed659f1",
+    "url": "/static/js/2.849ed1dc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.849ed1dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e0fe107ae2bff8fb306",
+    "url": "/static/js/main.56ddacbe.chunk.js"
+  },
+  {
+    "revision": "9212cdf8da1296d1f5bf",
+    "url": "/static/js/runtime-main.1d7d1311.js"
+  },
+  {
+    "revision": "d3f1e4320969b8acc3bd62a077b5911f",
+    "url": "/static/media/ang.d3f1e432.png"
+  },
+  {
+    "revision": "64fb7eb24c8febf235d1a1f5b9c92a63",
+    "url": "/static/media/astronomy.64fb7eb2.png"
+  },
+  {
+    "revision": "efc1aa329f933be3b1658505278685c6",
+    "url": "/static/media/bootstrap.efc1aa32.svg"
+  },
+  {
+    "revision": "deb69b7a3084688a86ff04d8ed2537f6",
+    "url": "/static/media/budo.deb69b7a.PNG"
+  },
+  {
+    "revision": "45340ae02eee56f142ca358a20d842f9",
+    "url": "/static/media/calculator.45340ae0.png"
+  },
+  {
+    "revision": "ce5e2139e71c80eca60f874f3b2eab82",
+    "url": "/static/media/cantor.ce5e2139.png"
+  },
+  {
+    "revision": "55338d57e6f6c200bcd23f0af4710a9b",
+    "url": "/static/media/css.55338d57.svg"
+  },
+  {
+    "revision": "6bb158442892aba22a790278af1a4ee3",
+    "url": "/static/media/diet.6bb15844.png"
+  },
+  {
+    "revision": "f3f4f15d89adeb50dac4487a1a9e4b18",
+    "url": "/static/media/git.f3f4f15d.svg"
+  },
+  {
+    "revision": "15a5a8bda0c1e730557fa45b6a2a941c",
+    "url": "/static/media/html-5.15a5a8bd.svg"
+  },
+  {
+    "revision": "bc786b35d99c52f347831dc995ab7143",
+    "url": "/static/media/js.bc786b35.svg"
+  },
+  {
+    "revision": "6b9e283a7c1a70ae25b3efa08781eafd",
+    "url": "/static/media/kissi.6b9e283a.PNG"
+  },
+  {
+    "revision": "b2ca1b5f50cbeb09f8afb8edfb647e32",
+    "url": "/static/media/lansoft.b2ca1b5f.png"
+  },
+  {
+    "revision": "7ed5645f74bec90040770b4d8106a758",
+    "url": "/static/media/letterts.7ed5645f.png"
+  },
+  {
+    "revision": "99442443fd0837d999a918740679479f",
+    "url": "/static/media/me.99442443.png"
+  },
+  {
+    "revision": "fa3a72589ef9b8eb0b4bb048e144a829",
+    "url": "/static/media/money.fa3a7258.png"
+  },
+  {
+    "revision": "f79f40aa3bd987d11c541fc5958da176",
+    "url": "/static/media/mysql.f79f40aa.svg"
+  },
+  {
+    "revision": "f6163a86aad1ae679301fac1a1948fc2",
+    "url": "/static/media/npm.f6163a86.svg"
+  },
+  {
+    "revision": "5f4952d85d434b7d00b7c493e3b79f97",
+    "url": "/static/media/php.5f4952d8.svg"
+  },
+  {
+    "revision": "1a2e6932e63ee4637912ab6d92eccce5",
+    "url": "/static/media/react.1a2e6932.svg"
+  },
+  {
+    "revision": "85772647236e76b8e772c14c092a28ac",
+    "url": "/static/media/redux.85772647.svg"
+  },
+  {
+    "revision": "ac379ea36de51cab9bb489aad8f6d2f5",
+    "url": "/static/media/saga.ac379ea3.svg"
+  },
+  {
+    "revision": "3023657df2a80a0f8957a339d14ef515",
+    "url": "/static/media/sass.3023657d.svg"
+  },
+  {
+    "revision": "c4f2cdc7ffaec1211e7ae07babdffda7",
+    "url": "/static/media/tlo.c4f2cdc7.png"
+  },
+  {
+    "revision": "3d6f357eb0b0e5fdec33b1843e2dd4e0",
+    "url": "/static/media/todo.3d6f357e.png"
+  },
+  {
+    "revision": "c306fa8a366806d20f208c1b274329af",
+    "url": "/static/media/wordpress.c306fa8a.svg"
+  }
+]);
