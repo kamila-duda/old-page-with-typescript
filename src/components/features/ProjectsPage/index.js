ce28c36a72b5c44d37f8ca5d84321ccf8f1bb0ef
kamila-duda <kakus86@o2.pl>
2020-11-02 15:49:27 +0100

    reduced code in Project Component; added new project

--- src/components/features/ProjectsPage/index.js
+++ src/components/features/ProjectsPage/index.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React from "react";
 import ReactCardCarousel from "react-card-carousel";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCode, faSearch } from "@fortawesome/free-solid-svg-icons";
@@ -15,264 +15,43 @@ import {
   StyledCard,
 } from "./styled";
 import { projects } from "./projects";
-import { TweenMax } from "gsap";
-import { Power3 } from "gsap";
 
 const ProjectsPage = () => {
-  let firstText = useRef();
-  let firstCarusel = useRef();
-  useEffect(() => {
-    console.log(firstText, firstCarusel)
-    TweenMax.staggerFrom(
-      [firstText, firstCarusel],
-      0.8,
-      { opacity: 0, x: -400, ease: Power3.easeOut },
-      0.5
-    );
-  });
   return (
     <Container>
       <StyledSubtitle>My projects:</StyledSubtitle>
-      <StyledArticle>
-        <StyledTextContainer ref={(el) => (firstText = el)}>
-          <h3>Games</h3>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-          bibendum lectus magna, ac auctor est commodo eu. In tristique nunc vel
-          sem egestas ornare. Morbi euismod lobortis magna at viverra. Cras eros
-          diam, semper et orci a, molestie efficitur enim. Sed in augue libero.
-          Proin ac ex ac arcu pretium dignissim. Interdum et malesuada fames ac
-          ante ipsum primis in faucibus. Quisque ligula erat, posuere vel lacus
-          eget, bibendum sagittis urna.
-        </StyledTextContainer>
-        <StyledImagesContainer ref={(el) => (firstCarusel = el)}>
-          <ReactCardCarousel>
-            <StyledCard>
-              <StyledImage src={projects.games.one.money} alt="" />
-              <StyledDetailsContainer>
-                <StyledLink href={projects.games.one.code}>
-                  <FontAwesomeIcon icon={faCode} />
-                </StyledLink>
-                <StyledLink href={projects.games.one.link}>
-                  <FontAwesomeIcon icon={faSearch} />
-                </StyledLink>
-              </StyledDetailsContainer>
-              <StyledDetailsContainer>
-                {projects.games.one.tags.map((tag) => (
-                  <>
-                    <StyledDetail>{tag}</StyledDetail>
-                  </>
-                ))}
-              </StyledDetailsContainer>
-            </StyledCard>
-            <StyledCard>
-              <StyledImage src={projects.games.two.letters} alt="" />
-              <StyledDetailsContainer>
-                <StyledLink href={projects.games.two.code}>
-                  <FontAwesomeIcon icon={faCode} />
-                </StyledLink>
-                <StyledLink href={projects.games.two.link}>
-                  <FontAwesomeIcon icon={faSearch} />
-                </StyledLink>
-              </StyledDetailsContainer>
-              <StyledDetailsContainer>
-                {projects.games.two.tags.map((tag) => (
-                  <>
-                    <StyledDetail>{tag}</StyledDetail>
-                  </>
-                ))}
-              </StyledDetailsContainer>
-            </StyledCard>
-          </ReactCardCarousel>
-        </StyledImagesContainer>
-      </StyledArticle>
-      <StyledArticle>
-        <StyledTextContainer>
-          <h3>Websites</h3>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-          bibendum lectus magna, ac auctor est commodo eu. In tristique nunc vel
-          sem egestas ornare. Morbi euismod lobortis magna at viverra. Cras eros
-          diam, semper et orci a, molestie efficitur enim. Sed in augue libero.
-          Proin ac ex ac arcu pretium dignissim. Interdum et malesuada fames ac
-          ante ipsum primis in faucibus. Quisque ligula erat, posuere vel lacus
-          eget, bibendum sagittis urna.
-        </StyledTextContainer>
-        <StyledImagesContainer>
-          <ReactCardCarousel>
-            <StyledCard>
-              <StyledImage src={projects.website.one.diet} alt="" />
-              <StyledDetailsContainer>
-                <StyledLink href={projects.website.one.code}>
-                  <FontAwesomeIcon icon={faCode} />
-                </StyledLink>
-                <StyledLink href={projects.website.one.link}>
-                  <FontAwesomeIcon icon={faSearch} />
-                </StyledLink>
-              </StyledDetailsContainer>
-              <StyledDetailsContainer>
-                {projects.website.one.tags.map((tag) => (
-                  <>
-                    <StyledDetail>{tag}</StyledDetail>
-                  </>
-                ))}
-              </StyledDetailsContainer>
-            </StyledCard>
-            <StyledCard>
-              <StyledImage src={projects.website.two.astronomy} alt="" />
-              <StyledDetailsContainer>
-                <StyledLink href={projects.website.two.code}>
-                  <FontAwesomeIcon icon={faCode} />
-                </StyledLink>
-                <StyledLink href={projects.website.two.link}>
-                  <FontAwesomeIcon icon={faSearch} />
-                </StyledLink>
-              </StyledDetailsContainer>
-              <StyledDetailsContainer>
-                {projects.website.two.tags.map((tag) => (
-                  <>
-                    <StyledDetail>{tag}</StyledDetail>
-                  </>
-                ))}
-              </StyledDetailsContainer>
-            </StyledCard>
-            <StyledCard>
-              <StyledImage src={projects.website.three.lansoft} alt="" />
-              <StyledDetailsContainer>
-                <StyledLink href={projects.website.three.code}>
-                  <FontAwesomeIcon icon={faCode} />
-                </StyledLink>
-                <StyledLink href={projects.website.three.link}>
-                  <FontAwesomeIcon icon={faSearch} />
-                </StyledLink>
-              </StyledDetailsContainer>
-              <StyledDetailsContainer>
-                {projects.website.three.tags.map((tag) => (
-                  <>
-                    <StyledDetail>{tag}</StyledDetail>
-                  </>
-                ))}
-              </StyledDetailsContainer>
-            </StyledCard>
-            <StyledCard>
-              <StyledImage src={projects.website.four.kissi} alt="" />
-              <StyledDetailsContainer>
-                <StyledLink href={projects.website.four.link}>
-                  <FontAwesomeIcon icon={faSearch} />
-                </StyledLink>
-              </StyledDetailsContainer>
-              <StyledDetailsContainer>
-                {projects.website.four.tags.map((tag) => (
-                  <>
-                    <StyledDetail>{tag}</StyledDetail>
-                  </>
-                ))}
-              </StyledDetailsContainer>
-            </StyledCard>
-            <StyledCard>
-              <StyledImage src={projects.website.five.budo} alt="" />
-              <StyledDetailsContainer>
-                <StyledLink href={projects.website.five.link}>
-                  <FontAwesomeIcon icon={faSearch} />
-                </StyledLink>
-              </StyledDetailsContainer>
-              <StyledDetailsContainer>
-                {projects.website.five.tags.map((tag) => (
-                  <>
-                    <StyledDetail>{tag}</StyledDetail>
-                  </>
-                ))}
-              </StyledDetailsContainer>
-            </StyledCard>
-          </ReactCardCarousel>
-        </StyledImagesContainer>
-      </StyledArticle>
-      <StyledArticle>
-        <StyledTextContainer>
-          <h3>Other</h3>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-          bibendum lectus magna, ac auctor est commodo eu. In tristique nunc vel
-          sem egestas ornare. Morbi euismod lobortis magna at viverra. Cras eros
-          diam, semper et orci a, molestie efficitur enim. Sed in augue libero.
-          Proin ac ex ac arcu pretium dignissim. Interdum et malesuada fames ac
-          ante ipsum primis in faucibus. Quisque ligula erat, posuere vel lacus
-          eget, bibendum sagittis urna.
-        </StyledTextContainer>
-        <StyledImagesContainer>
-          <ReactCardCarousel>
-            <StyledCard>
-              <StyledImage src={projects.other.one.cantor} alt="" />
-              <StyledDetailsContainer>
-                <StyledLink href={projects.other.one.code}>
-                  <FontAwesomeIcon icon={faCode} />
-                </StyledLink>
-                <StyledLink href={projects.other.one.link}>
-                  <FontAwesomeIcon icon={faSearch} />
-                </StyledLink>
-              </StyledDetailsContainer>
-              <StyledDetailsContainer>
-                {projects.other.one.tags.map((tag) => (
-                  <>
-                    <StyledDetail>{tag}</StyledDetail>
-                  </>
-                ))}
-              </StyledDetailsContainer>
-            </StyledCard>
-            <StyledCard>
-              <StyledImage src={projects.other.two.todo} alt="" />
-              <StyledDetailsContainer>
-                <StyledLink href={projects.other.two.code}>
-                  <FontAwesomeIcon icon={faCode} />
-                </StyledLink>
-                <StyledLink href={projects.other.two.link}>
-                  <FontAwesomeIcon icon={faSearch} />
-                </StyledLink>
-              </StyledDetailsContainer>
-              <StyledDetailsContainer>
-                {projects.other.two.tags.map((tag) => (
-                  <>
-                    <StyledDetail>{tag}</StyledDetail>
-                  </>
-                ))}
-              </StyledDetailsContainer>
-            </StyledCard>
-            <StyledCard>
-              <StyledImage src={projects.other.three.ang} alt="" />
-              <StyledDetailsContainer>
-                <StyledLink href={projects.other.three.code}>
-                  <FontAwesomeIcon icon={faCode} />
-                </StyledLink>
-                <StyledLink href={projects.other.three.link}>
-                  <FontAwesomeIcon icon={faSearch} />
-                </StyledLink>
-              </StyledDetailsContainer>
-              <StyledDetailsContainer>
-                {projects.other.three.tags.map((tag) => (
-                  <>
-                    <StyledDetail>{tag}</StyledDetail>
-                  </>
-                ))}
-              </StyledDetailsContainer>
-            </StyledCard>
-            <StyledCard>
-              <StyledImage src={projects.other.four.calculator} alt="" />
-              <StyledDetailsContainer>
-                <StyledLink href={projects.other.four.code}>
-                  <FontAwesomeIcon icon={faCode} />
-                </StyledLink>
-                <StyledLink href={projects.other.four.link}>
-                  <FontAwesomeIcon icon={faSearch} />
-                </StyledLink>
-              </StyledDetailsContainer>
-              <StyledDetailsContainer>
-                {projects.other.four.tags.map((tag) => (
-                  <>
-                    <StyledDetail>{tag}</StyledDetail>
-                  </>
-                ))}
-              </StyledDetailsContainer>
-            </StyledCard>
-          </ReactCardCarousel>
-        </StyledImagesContainer>
-      </StyledArticle>
+      {projects.map((category) => (
+        <StyledArticle>
+          <StyledTextContainer>
+            <h3>{category.title}</h3>
+            <p>Bla</p>
+          </StyledTextContainer>
+          <StyledImagesContainer>
+            <ReactCardCarousel>
+              {category.items.map((item) => (
+                <StyledCard>
+                  <StyledImage src={item.image} alt="" />
+                  <StyledDetailsContainer>
+                    <StyledLink href={item.code}>
+                      <FontAwesomeIcon icon={faCode} />
+                    </StyledLink>
+                    <StyledLink href={item.link}>
+                      <FontAwesomeIcon icon={faSearch} />
+                    </StyledLink>
+                  </StyledDetailsContainer>
+                  <StyledDetailsContainer>
+                    {item.tags.map((tag) => (
+                      <>
+                        <StyledDetail>{tag}</StyledDetail>
+                      </>
+                    ))}
+                  </StyledDetailsContainer>
+                </StyledCard>
+              ))}
+            </ReactCardCarousel>
+          </StyledImagesContainer>
+        </StyledArticle>
+      ))}
     </Container>
   );
 };
--- src/components/features/ProjectsPage/projects.js
+++ src/components/features/ProjectsPage/projects.js
@@ -9,77 +9,103 @@ import calculator from "./../../../images/calculator.png";
 import todo from "./../../../images/todo.png";
 import kissi from "./../../../images/kissi.PNG";
 import budo from "./../../../images/budo.PNG";
+import movies from "./../../../images/movies.PNG";
 
+export const projects = [
+  {
+    title: "Web applications",
+    items: [
+      {
+        image: movies,
+        code: "https://github.com/kamila-duda/movies-browser",
+        link: "https://kamila-duda.github.io/movies-browser/",
+        tags: ["React.js", "Styled Components", "Redux", "Saga"],
+      },
+      {
+        image: todo,
+        code: "https://github.com/kamila-duda/todo-list-react-redux",
+        link: "https://kamila-duda.github.io/todo-list-react-redux/",
+        tags: ["React.js", "StyledComponents", "Redux", "Saga"],
+      },
+      {
+        image: cantor,
+        code: "https://github.com/kamila-duda/currency-converter-react",
+        link: "https://kamila-duda.github.io/currency-converter-react/",
+        tags: ["React.js", "Styled Components"],
+      },
+    ],
+  },
+  {
+    title: "Games",
+    items: [
+      {
+        image: money,
+        code: "https://github.com/kamila-duda/coins",
+        link: "https://kamila-duda.github.io/coins/",
+        tags: ["HTML", "CSS", "Sass", "JavaScript"],
+      },
+      {
+        image: letters,
+        code: "https://github.com/kamila-duda/letters_game",
+        link: "https://kamila-duda.github.io/letters_game/",
+        tags: ["HTML", "CSS", "Sass", "JavaScript"],
+      },
+      {
+        image: ang,
+        code: "https://github.com/kamila-duda/angielski_quiz",
+        link: "https://kamila-duda.github.io/angielski_quiz/",
+        tags: ["HTML", "CSS", "JavaScript"],
+      },
+    ],
+  },
+  {
+    title: "Websites",
+    items: [
+      {
+        image: diet,
+        code: "https://github.com/kamila-duda/dieta",
+        link: "https://kamila-duda.github.io/dieta/",
+        tags: ["HTML", "CSS", "Bootstrap", "JavaScript"],
+      },
+      {
+        image: astronomy,
+        code: "https://github.com/kamila-duda/astronomy",
+        link: "https://kamila-duda.github.io/astronomy/",
+        tags: ["HTML", "CSS", "JavaScript"],
+      },
+      {
+        image: lansoft,
+        code: "https://github.com/kamila-duda/lansoft",
+        link: "https://kamila-duda.github.io/lansoft/",
+        tags: ["HTML", "CSS", "Sass", "JavaScript"],
+      },
+      {
+        image: kissi,
+        link: "http://www.iisi.pcz.pl/~pduda/index.php",
+        tags: ["HTML", "CSS", "Bootstrap", "JavaScript", "PHP", "MySQL"],
+      },
+      {
+        image: budo,
+        link: "http://kd2020-pl.stackstaging.com/",
+        tags: ["HTML", "CSS", "Bootstrap", "JavaScript"],
+      },
+    ],
+  },
+  {
+    title: "Other",
+    items: [
+      // {
+      //  image: wordpress,
+      //   link: "https://nawrot.000webhostapp.com/",
+      //   tags: ["Wordpress"]
 
-export const projects = {
-  games: {
-    one: {
-      money,
-      code: "https://github.com/kamila-duda/coins",
-      link: "https://kamila-duda.github.io/coins/",
-      tags: ["HTML", "CSS", "Sass", "JavaScript"] 
-    },
-    two: {
-      letters,
-      code: "https://github.com/kamila-duda/letters_game",
-      link: "https://kamila-duda.github.io/letters_game/",
-      tags: ["HTML", "CSS", "Sass", "JavaScript"]
-    },
+      // },
+      {
+        image: calculator,
+        code: "https://github.com/kamila-duda/calculator",
+        link: "https://kamila-duda.github.io/calculator",
+        tags: ["HTML", "CSS", "JavaScript"],
+      },
+    ],
   },
-  website: {
-    one: {
-      diet,
-      code: "https://github.com/kamila-duda/dieta",
-      link: "https://kamila-duda.github.io/dieta/",
-      tags: ["HTML", "CSS", "Bootstrap", "JavaScript"]
-    },
-    two: {
-      astronomy,
-      code: "https://github.com/kamila-duda/astronomy",
-      link: "https://kamila-duda.github.io/astronomy/",
-      tags: ["HTML", "CSS", "JavaScript"]
-    },
-    three: {
-      lansoft,
-      code: "https://github.com/kamila-duda/lansoft",
-      link: "https://kamila-duda.github.io/lansoft/",
-      tags: ["HTML", "CSS", "Sass", "JavaScript"]
-    },
-    four: {
-      kissi,
-      link: "http://www.iisi.pcz.pl/~pduda/index.php",
-      tags: ["HTML", "CSS", "Bootstrap", "JavaScript", "PHP", "MySQL"]
-    },
-    five: {
-      budo,
-      link: "http://kd2020-pl.stackstaging.com/",
-      tags: ["HTML", "CSS", "Bootstrap", "JavaScript"]
-    }
-  },
-  other: {
-    one: {
-      cantor,
-      code: "https://github.com/kamila-duda/currency-converter-react",
-      link: "https://kamila-duda.github.io/currency-converter-react/",
-      tags: ["React.js", "Styled Components"]
-    },
-    two: {
-      todo,
-      code: "https://github.com/kamila-duda/todo-list-react-redux",
-      link: "https://kamila-duda.github.io/todo-list-react-redux/",
-      tags: ["React.js", "StyledComponents", "Redux", "Saga"]
-    },
-    three: {
-      ang,
-      code: "https://github.com/kamila-duda/angielski_quiz",
-      link: "https://kamila-duda.github.io/angielski_quiz/",
-      tags: ["HTML", "CSS", "JavaScript"]
-    },
-    four: {
-      calculator,
-      code: "https://github.com/kamila-duda/calculator",
-      link: "https://kamila-duda.github.io/calculator",
-      tags: ["HTML", "CSS", "JavaScript"]
-    }
-  }
-};
+];
